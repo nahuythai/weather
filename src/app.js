20e92679b665aebc336baadec6a030c52b67fe1b
Tides <huythaislna@gmail.com>
2020-02-26 08:21:17 +0700

    Config for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('../utils/geocode')
 const weather = require('../utils/weather')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 app.set('view engine', 'hbs')
@@ -56,6 +57,6 @@ app.get('/*', (req, res) => {
     res.render('404')
 })
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000")
+app.listen(port, () => {
+    console.log("Server is up on port " + port)
 })
